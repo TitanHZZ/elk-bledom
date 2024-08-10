4eb07ea87835f09c79b663e027da89d362e47691
TitanHZZ <titanhzz2018@gmail.com>
2024-08-10 14:28:02 +0100

    Fixed set_power() and started set_mode().

--- bledom.py
+++ bledom.py
@@ -13,6 +13,54 @@ SERVICE_UUID = "0000fff0-0000-1000-8000-00805f9b34fb"
 CHARACTERISTIC_UUID = "0000fff3-0000-1000-8000-00805f9b34fb"
 MAX_CONNECTIONS = 20
 
+EFFECT_0  = "three color jump"
+EFFECT_1  = "seven color jump"
+EFFECT_2  = "three color cross fade"
+EFFECT_3  = "seven color cross fade"
+EFFECT_4  = "red fade"
+EFFECT_5  = "green fade"
+EFFECT_6  = "blue fade"
+EFFECT_7  = "yellow fade"
+EFFECT_8  = "cyan fade"
+EFFECT_9  = "magenta fade"
+EFFECT_10 = "white fade"
+EFFECT_11 = "red green cross fade"
+EFFECT_12 = "red blue cross fade"
+EFFECT_13 = "green blue cross fade"
+EFFECT_14 = "seven color strobe flash"
+EFFECT_15 = "red strobe flash"
+EFFECT_16 = "green strobe flash"
+EFFECT_17 = "blue strobe flash"
+EFFECT_18 = "yellow strobe flash"
+EFFECT_19 = "cyan strobe flash"
+EFFECT_20 = "magenta strobe flash"
+EFFECT_21 = "white strobe flash"
+
+MODES = {
+    EFFECT_0: 0x87,
+    EFFECT_1: 0x88,
+    EFFECT_2: 0x89,
+    EFFECT_3: 0x8a,
+    EFFECT_4: 0x8b,
+    EFFECT_5: 0x8c,
+    EFFECT_6: 0x8d,
+    EFFECT_7: 0x8e,
+    EFFECT_8: 0x8f,
+    EFFECT_9: 0x90,
+    EFFECT_10: 0x91,
+    EFFECT_11: 0x92,
+    EFFECT_12: 0x93,
+    EFFECT_13: 0x94,
+    EFFECT_14: 0x95,
+    EFFECT_15: 0x96,
+    EFFECT_16: 0x97,
+    EFFECT_17: 0x98,
+    EFFECT_18: 0x99,
+    EFFECT_19: 0x9a,
+    EFFECT_20: 0x9b,
+    EFFECT_21: 0x9c
+}
+
 class ElkBledomLight:
     def __init__(self, address):
         self.client = None
@@ -28,8 +76,6 @@ class ElkBledomLight:
                 await self.client.connect()
                 print(f"Connected to ELK_BLEDOM rgb strip with address {self.address}!")
                 self.connected = True
-
-                await self.set_free()
             except:
                 pass
 
@@ -49,36 +95,43 @@ class ElkBledomLight:
         await self.client.write_gatt_char(CHARACTERISTIC_UUID, color, response=True)
 
     async def set_brightness(self, br):
-        br = bytearray([0x7e, 0x04, 0x01, br, 0x01, 0xff, 0x02, 0x01, 0xef])
+        assert br >= 0 and br <= 100
+        br = bytearray([0x7e, 0x04, 0x01, br, 0x01, 0xff, 0xff, 0x00, 0xef])
         await self.client.write_gatt_char(CHARACTERISTIC_UUID, br, response=True)
 
-    # async def set_power(self, power):
-    #     pw = bytearray([0x7e, 0x00, 0x04, 0xf0, 0x00, 0x01, 0xff, 0x00, 0xef] if power else [0x7e, 0x00, 0x04, 0x00, 0x00, 0x00, 0xff, 0x00, 0xef])
-    #     await self.client.write_gatt_char(CHARACTERISTIC_UUID, pw, response=True)
+    async def set_power(self, power):
+        pw = bytearray([0x7e, 0x04, 0x04, 0xf0, 0x00, 0x01, 0xff, 0x00, 0xef] if power else [0x7e, 0x04, 0x04, 0x00, 0x00, 0x00, 0xff, 0x00, 0xef])
+        await self.client.write_gatt_char(CHARACTERISTIC_UUID, pw, response=True)
+
+    # async def set_mode(self, mode):
+    #     assert mode >= 0x87 and mode <= 0x9c
+    #     md = bytearray([0x7e, 0x00, 0x03, mode, 0x03, 0x00, 0x00, 0x00, 0xef])
+    #     await self.client.write_gatt_char(CHARACTERISTIC_UUID, md, response=True)
 
 async def main():
     light = ElkBledomLight(DEVICE_ADDR)
     await light.connect()
 
-    await light.set_color("FF0000")
-    await asyncio.sleep(0.5)
-    await light.set_color("00FF00")
-    await asyncio.sleep(0.5)
-    await light.set_color("0000FF")
+    # await light.set_color("FF0000")
+    # await asyncio.sleep(0.5)
+    # await light.set_color("00FF00")
+    # await asyncio.sleep(0.5)
+    # await light.set_color("0000FF")
 
-    await asyncio.sleep(1)
-
-    await light.set_brightness(50)
-    await asyncio.sleep(1)
-    await light.set_brightness(100)
-
-    await asyncio.sleep(1)
-
-    # await light.set_power(False)
     # await asyncio.sleep(1)
-    # await light.set_power(True)
+
+    # await light.set_brightness(50)
+    # await asyncio.sleep(1)
+    # await light.set_brightness(100)
+
+    # await asyncio.sleep(1)
+
+    # await light.set_mode(0x87)
+
+    await light.set_power(False)
+    await asyncio.sleep(1)
+    await light.set_power(True)
 
     await light.disconnect()
 
-# Run the async main function
 asyncio.run(main())
